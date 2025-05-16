c97ca6a3298eaf8ca6e5dbc0043ed66f6a15fc79
euphoo-01 <euphoo86@gmail.com>
2025-05-16 17:26:50 +0300

    Соответствие общему дизайну

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  const [page, setPage] = useState("0");
-  return (
-    <ThemeProvider>
-      <AuthProvider>
-        <div className="background-elements">
-          <div className="circle circle-1"></div>
-          <div className="circle circle-2"></div>
-          <div className="circle circle-3"></div>
-          <div className="circle circle-4"></div>
-        </div>
-        {page === "0" && <AuthorizationPage pageHandler={setPage} />}
-        {page === "1" && <MainPage pageHandler={setPage}/>}
-      </AuthProvider>
-    </ThemeProvider>
-  );
+	const [page, setPage] = useState("0");
+	return (
+		<ThemeProvider>
+			<AuthProvider>
+				<div className="background-elements">
+					<div className="circle circle-1"></div>
+					<div className="circle circle-2"></div>
+					<div className="circle circle-3"></div>
+					<div className="circle circle-4"></div>
+				</div>
+				{page === "0" && <AuthorizationPage pageHandler={setPage} />}
+				{page === "1" && <MainPage pageHandler={setPage} />}
+			</AuthProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
